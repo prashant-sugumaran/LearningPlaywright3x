bb8e313c477ddd85259058b7c83a3b826ce06b92
prashant-sugumaran <prashantsugumaran@gmail.com>
2026-08-11 18:29:14 +0530

    new commit

---
+++ 05_chapter_Operator/22_ternnary.js
@@ -0,0 +1,15 @@
+let age = 20;
+let res = age > 18 ? 'Yes' : 'No';
+console.log(res);
+
+let actualStatuCode = 200;
+let expectedStatusCode = 200;
+
+let testRes = actualStatuCode === expectedStatusCode ? 'Pass' : 'Fail';
+console.log(testRes);
+
+let actRes = 200;
+let expRes = 500;
+
+let res1 = actRes === expRes ? 'Pass' : 'Fail';
+console.log(res1);

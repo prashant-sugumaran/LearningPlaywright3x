d5f38df14cd8f9983d5bbf9fd5f75a790f2f78e0
prashant-sugumaran <prashantsugumaran@gmail.com>
2026-08-12 17:44:56 +0530

    new commit

---
+++ 06_chapter_Statement/37_IQ.js
@@ -0,0 +1,14 @@
+
+// let age = 20;
+
+// if(age > 18){
+//     console.log('go goa');
+// }
+// else{
+//     console.log('no goa');
+// }
+
+let age = 10;
+
+let res = age > 18? 'go goa' : 'no goa';
+console.log(res);
